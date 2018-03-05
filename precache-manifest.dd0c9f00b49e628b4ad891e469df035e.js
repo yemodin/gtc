59db42045b568da551e162af36c56b09c8a7f62e
Mikhail Vashlyaev <yemodin@yemodin.com>
2018-03-06 03:31:42 +0500

    release 0.0.1

---
+++ precache-manifest.dd0c9f00b49e628b4ad891e469df035e.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ff757bd3ca39d84379fc742aab3a2ff",
+    "url": "/gtc/assets/favicon/mstile-150x150.png"
+  },
+  {
+    "revision": "d4fc79d0863011d7acac",
+    "url": "/gtc/js/vendor.d4fc79d0.js"
+  },
+  {
+    "revision": "163e8bed842e61841ed183a49711653a",
+    "url": "/gtc/assets/favicon/mstile-310x310.png"
+  },
+  {
+    "revision": "ac6d4a8ec8070c82b2f1",
+    "url": "/gtc/css/app.482d70a0.css"
+  },
+  {
+    "revision": "de284c1dcdb30c7955836a3ec03815c6",
+    "url": "/gtc/index.html"
+  },
+  {
+    "revision": "a7981a364558b6a05748",
+    "url": "/gtc/js/manifest.a7981a36.js"
+  },
+  {
+    "revision": "ac6d4a8ec8070c82b2f1",
+    "url": "/gtc/js/app.ac6d4a8e.js"
+  },
+  {
+    "revision": "cc162729d771e0743b884c4d5dd5ebd0",
+    "url": "/gtc/assets/icon-add.svg"
+  },
+  {
+    "revision": "51326234ad6bce4fba956f85f804a98f",
+    "url": "/gtc/browserconfig.xml"
+  },
+  {
+    "revision": "e8d6af19fce8b3809418edc2ab982322",
+    "url": "/gtc/assets/icon-cog.svg"
+  },
+  {
+    "revision": "5a40b26e37fa8ec3caaf7bc668565a32",
+    "url": "/gtc/assets/favicon/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0edb952921a21722fadf04eb68c7c6a6",
+    "url": "/gtc/assets/favicon/apple-touch-icon.png"
+  },
+  {
+    "revision": "e3fd08897fb5337bfd8bfac8f3deb8b0",
+    "url": "/gtc/assets/favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "172e8c7c0cde987826ca5a60042e9b11",
+    "url": "/gtc/assets/favicon/favicon-194x194.png"
+  },
+  {
+    "revision": "b1c2e42e55c46d5299019aa569993899",
+    "url": "/gtc/assets/favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "f28d3c53a20fe93ba2d47c61c4e60ee9",
+    "url": "/gtc/assets/favicon/mstile-144x144.png"
+  },
+  {
+    "revision": "2ca23ef16c15967e62cba1f68cb9f272",
+    "url": "/gtc/assets/logo.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/gtc/assets/logo.png"
+  },
+  {
+    "revision": "f59911eaae55afe0de79067acc733adf",
+    "url": "/gtc/assets/favicon/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "e3f70d38f4bbeeb714032b259e013725",
+    "url": "/gtc/assets/logo-color.svg"
+  },
+  {
+    "revision": "0874abd52d9dce16b49faf6aaa33f863",
+    "url": "/gtc/assets/favicon/android-chrome-256x256.png"
+  },
+  {
+    "revision": "9e6c92b8c9610831d1e3de2d577b0bfe",
+    "url": "/gtc/assets/icon-github.svg"
+  },
+  {
+    "revision": "890ea459738a55e74c46b3ec9ddcb1fe",
+    "url": "/gtc/assets/icon-home.svg"
+  },
+  {
+    "revision": "5328003b66db913b085637d02aa813a3",
+    "url": "/gtc/assets/icon-menu.svg"
+  },
+  {
+    "revision": "b453b8aec39b2b4759621a04788915ab",
+    "url": "/gtc/assets/icon-thumbs-up.svg"
+  },
+  {
+    "revision": "801d49b09bf532baac608cba28634712",
+    "url": "/gtc/assets/icon-verified.svg"
+  },
+  {
+    "revision": "6613de24a4d4b1bcc192cf730437d872",
+    "url": "/gtc/assets/icon-whatshot.svg"
+  },
+  {
+    "revision": "5453e3ceee3ee37909e6a67d18a0bbee",
+    "url": "/gtc/assets/logo-color.png"
+  },
+  {
+    "revision": "c167a7662305d9a0969d8657b1593723",
+    "url": "/gtc/assets/boilerplates/webpack.png"
+  },
+  {
+    "revision": "ad38adc923567524e3efec9e1088d3eb",
+    "url": "/gtc/assets/boilerplates/nuxt.png"
+  },
+  {
+    "revision": "a3aa41525b5acfac526815bba7ed1467",
+    "url": "/gtc/assets/boilerplates/html5.png"
+  },
+  {
+    "revision": "d1619c4316133ade9fb07ae3c4f8fb4c",
+    "url": "/gtc/assets/boilerplates/codepen.png"
+  }
+];
